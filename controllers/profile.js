24780f65bf24b758ffcd3533c8704957db219a2e
Paradigm <ParadigmEncryption@gmail.com>
2019-05-06 13:21:31 -0500

    adding starting project api

---
+++ controllers/profile.js
@@ -0,0 +1,16 @@
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        throw new Error('Could not find that user');
+      }  
+    })
+  .catch(err => res.status(400).json(err.message))
+}
+
+module.exports = {
+  handleProfileGet
+}
